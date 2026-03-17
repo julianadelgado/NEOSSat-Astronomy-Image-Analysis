c0faab7a7c9981f20389b270e95dcac8b6decd4a
Khalil Krayem <107659473+Kralilu@users.noreply.github.com>
2026-03-17 11:28:55 -0400

    Adding streak-detection to main (#11)
    * all the stuff
    
    * fix dependencies
    
    * only check for python 3.11 on ci
    
    * lint
    
    * comments
    
    * adding param
    
    * splitting code
    
    * format-lint
    
    * lint
    
    * stuff

--- cli/main.py
+++ cli/main.py
@@ -9,6 +9,10 @@ from acquisition.data_manager import DataManager
 from acquisition.fits_handler import FitsHandler
 from acquisition.image_stacking import ImageStacking
 from cli.validator import validate_data_directory, validate_email
+
+from detectors.dl_streak_detector import DLStreakDetector
+from preprocessing.metrics import Metrics
+from preprocessing.pipeline import Pipeline
 from preprocessing.preprocessors.star_detection import StarDetection
 
 from .config import load_config
@@ -122,7 +126,8 @@ def main(
 
     if run_streaks:
         print("Running streak detection...")
-        # TODO Call streak detection function here
+        detector = DLStreakDetector(data_dir=cfg.data_dir, clean_results=True)
+        results = detector.run()
 
 
 if __name__ == "__main__":
